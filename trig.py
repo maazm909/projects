bb531e1a882accbb2765bbac69744bba20e11a76
Maaz Mohammad <moha0736@umn.edu>
2019-10-09 11:29:50 -0500

    added plan and starting print branch

--- trig.py
+++ trig.py
@@ -1,1 +1,13 @@
-input()
+#sin(A+B) = sinAcosB + cosAsinB
+# have to ask type of trig function
+#then what the numbers are
+# print out answer
+
+def types():
+    print("What type of trig function would you like to use?")
+    print("1. Sum and Difference Formula (ex. sin(A+B) )")
+    print("2. Double Angle Formula (ex. sin(2A) )")
+    print("3. Half Angle Formula (ex. sin(A/2) )")
+    print("4. Product to Sum (ex. cosA*cosB) )")
+    print("5. Sum to Product(ex. cosA + cosB) )")
+    
