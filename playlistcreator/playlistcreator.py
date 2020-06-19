dfe89c4e001b97b378aa6a817d38ddedb3446c74
Maaz Mohammad <maazm909@gmail.com>
2020-06-19 16:20:43 -0500

    filled get_spotify_uri and get_youtube_client

--- playlistcreator/playlistcreator.py
+++ playlistcreator/playlistcreator.py
@@ -1,17 +1,39 @@
 import json
 import requests
+import os
+
 from secrets import spotify_user_id, spotify_token
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
 class Playlistcreator:
     """docstring for CreatePlaylist."""
 
     def __init__(self, arg):
-        super(Playlistcreator, self).__init__()
-        self.arg = arg
+        self.user_id = spotify_user_id
+        self.spotify_token = spotify_token
+        self.youtube_client = self.get_youtube_client()
 
     def get_youtube_client(self):
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+
+        youtube_client = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+        return youtube_client
+
 
     def get_liked_videos(self):
+        pass
 
     def create_playlist(self):
         request_body = json.dumps
@@ -32,6 +54,28 @@ class Playlistcreator:
             }
         )
 
+        response_json = response.json()
+
+        return response_json["id"];
+
+
     def get_spotify_uri(self):
+        query = "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=-7".format(song_name, artist)
+
+        response = requests.get(
+        query,
+        headers={
+            "Content-Type":"application/json",
+            "Authorization":"Bearer {}".format(spotify_token)
+            }
+        )
+
+        response_json = response.json()
+        songs = response_json["tracks"]["items"]
+
+        uri = songs[0]["uri"]
+
+        return uri
 
     def add_song_to_playlist(self):
+        pass
