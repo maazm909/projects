8c870da472aabffd87c0cdddcd781e928b7235cc
Maaz Mohammad <maazm909@gmail.com>
2020-06-19 14:55:45 -0500

    created file and skeleton

---
+++ playlistcreator/playlistcreator.py
@@ -0,0 +1,17 @@
+class Playlistcreator:
+    """docstring for CreatePlaylist."""
+
+    def __init__(self, arg):
+        super(Playlistcreator, self).__init__()
+        self.arg = arg
+
+    def get_youtube_client(self):
+
+    def get_liked_videos(self):
+
+    def create_playlist(self):
+
+    def get_spotify_uri(self):
+
+    def add_song_to_playlist(self):
+        
