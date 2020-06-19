e65e8debab0562eaebb11be1e6a66a7cdbf3c15e
Maaz Mohammad <maazm909@gmail.com>
2020-06-19 16:32:50 -0500

    filled get_liked_videos

--- playlistcreator/playlistcreator.py
+++ playlistcreator/playlistcreator.py
@@ -33,7 +33,28 @@ class Playlistcreator:
 
 
     def get_liked_videos(self):
-        pass
+        request = self.youtube_client.videos().list(
+            part = "snippet,contentDetails, statistics",
+            myRating="like"
+        )
+        response = request.exexute()
+
+        for item in response["items"]:
+            video_title = item["snippet"]["title"]
+            youtube_url = "https:www.youtube.com/watch?v={}".format(item["id"])
+
+            video = youtube_dl.YoutubeDL({}).extract_info(youtube_url, download=False)
+            song_name = video["track"]
+            artist = video["artist"]
+
+            self.all_song_info[video_title]=
+            {
+                "youtube_url":youtube_url,
+                "song_name": song_name,
+                "artist": artist,
+
+                "spotify_uri":self.get_spotify_uri(song_name,artist)
+            }
 
     def create_playlist(self):
         request_body = json.dumps
