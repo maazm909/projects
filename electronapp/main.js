1c0c81bc4848854f76358557dea3c71f1841cc6d
maazm909 <56097151+maazm909@users.noreply.github.com>
2022-08-23 12:47:05 -0500

    Added functionality for basic adding to spreadsheet Added functionality for reading name and ranges from spreadsheet into object

--- electronapp/main.js
+++ electronapp/main.js
@@ -1,20 +1,27 @@
 const {app, BrowserWindow} = require('electron');
+const { dirname } = require('path');
 const path = require('path');
 const url = require('url');
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width:800, height:600});
-
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
+  const win = new BrowserWindow({
+    width:800, 
+    height:600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false
     }
-  }));
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  // win.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }));
 
   win.webContents.openDevTools();
 
--- electronapp/renderer.js
+++ electronapp/renderer.js
@@ -1,7 +1,39 @@
+const ExcelJS  = require('exceljs');
+
 document.getElementById('ticketNum').addEventListener("input", addTicket);
+
+document.getElementById('submitbtn').addEventListener("click", processTickets);
+
+document.getElementById('submitbtn').addEventListener("click", loadRanges);
 
 let currentTickets = [];
 
+let ranges = {};
+
+//function to load ticket ranges of sellers
+async function loadRanges() {
+  var workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile("data/ranges.xlsx");
+  const sheet = workbook.worksheets[0];
+  sheet.eachRow(function(row, rowNumber) {
+    if (rowNumber != 1) { //need to skip first row, headers
+      let fullname = row.values[1];
+      let starts = row.values[2];
+      let ends = row.values[3];
+      let startspl = starts.split(",");
+      let endsspl = ends.split(",");
+      startspl = startspl.map(str => {
+        return Number(str);
+      });
+      endsspl = endsspl.map(str => {
+        return Number(str);
+      });
+      ranges = Object.assign({[fullname]:{'starts':startspl, 'ends':endsspl}}, ranges);
+    }
+  });
+}
+
+//adds ticket to list and to visual table
 function addTicket() {
   console.log("entered addticket");
   let number = document.getElementById('ticketNum').value;
@@ -38,9 +70,13 @@ function updateVisualList() {
     row.appendChild(cell);
     table.appendChild(row);
   }
-
 }
 
-function processTickets() {
-
+//takes whatever is in ticket list and adds it to spreadsheet
+async function processTickets() {
+  var workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile("data/testdata.xlsx");
+  var worksheet = workbook.worksheets[0];
+  worksheet.addRow([1, 'maaz', 'jeff']);
+  await workbook.xlsx.writeFile('data/testdata.xlsx');
 }
