2bea60c27cb0b0c949db8821bcc5d6860e3c4513
maazm909 <56097151+maazm909@users.noreply.github.com>
2024-02-07 23:01:12 -0600

    Added registration row component Day of view now has form of one row and submit

---
+++ conventionapp/convention-app/components/AttendeeRow.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center" align="center">
+      <v-col> <v-text-field label="First Name"></v-text-field> </v-col>
+      <v-col> <v-text-field label="Last Name"></v-text-field> </v-col>
+      <v-col> <v-text-field label="Age" :rules="rules"></v-text-field> </v-col>
+      <v-col cols="1">
+        <v-radio-group>
+          <v-radio label="Male" value="male"></v-radio>
+          <v-radio label="Female" value="female"></v-radio>
+        </v-radio-group>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup></script>
+
+<script lang="ts">
+export default defineNuxtComponent({
+  data: () => ({
+    rules: [
+      (value: string) => {
+        if (value) return true;
+
+        return "You must enter a first name.";
+      },
+    ],
+  }),
+});
+</script>
+
+<style></style>
--- conventionapp/convention-app/pages/registration/dayof.vue
+++ conventionapp/convention-app/pages/registration/dayof.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-sheet width="100%">
+        <v-form @submit.prevent>
+          <v-container fluid>
+            <v-row>
+              <AttendeeRow />
+            </v-row>
+            <v-row justify="center">
+              <v-btn type="submit">Submit</v-btn>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-sheet>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { defineComponent } from "vue";
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  data: () => ({}),
+});
+</script>
 
 <style></style>
