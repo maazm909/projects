a45a328a448505f8d4ffdba9c3661bf22937eeee
maazm909 <56097151+maazm909@users.noreply.github.com>
2024-03-26 01:07:07 -0500

    Row now emits event when modified, created info variable

--- conventionapp/convention-app/components/AttendeeRow.vue
+++ conventionapp/convention-app/components/AttendeeRow.vue
@@ -1,11 +1,24 @@
 <template>
   <v-container fluid>
     <v-row justify="center" align="center">
-      <v-col> <v-text-field label="First Name"></v-text-field> </v-col>
-      <v-col> <v-text-field label="Last Name"></v-text-field> </v-col>
-      <v-col> <v-text-field label="Age" :rules="rules"></v-text-field> </v-col>
-      <v-col cols="1">
-        <v-radio-group>
+      <v-col>
+        <v-text-field
+          label="First Name"
+          v-model="info.firstName"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field label="Last Name" v-model="info.lastName"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+          label="Age"
+          v-model.number="info.age"
+          type="number"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-radio-group v-model="info.gender">
           <v-radio label="Male" value="male"></v-radio>
           <v-radio label="Female" value="female"></v-radio>
         </v-radio-group>
@@ -14,11 +27,22 @@
   </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { IAttendeeData } from "~/interfaces";
+</script>
 
 <script lang="ts">
 export default defineNuxtComponent({
+  props: {
+    rowIndex: Number,
+  },
   data: () => ({
+    info: {
+      firstName: "",
+      lastName: "",
+      age: 0,
+      gender: "male",
+    } as IAttendeeData,
     rules: [
       (value: string) => {
         if (value) return true;
@@ -27,6 +51,20 @@ export default defineNuxtComponent({
       },
     ],
   }),
+  mounted() {
+    // emit info to parent component to track
+    this.$emit("infoChange", this.info, this.rowIndex);
+  },
+  watch: {
+    info: {
+      handler() {
+        console.log("changeup");
+        this.$emit("infoChange", this.info, this.rowIndex);
+      },
+      deep: true,
+    },
+  },
+  emits: ["infoChange"],
 });
 </script>
 
