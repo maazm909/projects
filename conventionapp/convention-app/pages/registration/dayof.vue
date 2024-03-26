c7eb991948bfb6add03d12683f2ac1be7d09ea62
maazm909 <56097151+maazm909@users.noreply.github.com>
2024-03-26 01:07:38 -0500

    Added handling of row update and push of info to db

--- conventionapp/convention-app/pages/registration/dayof.vue
+++ conventionapp/convention-app/pages/registration/dayof.vue
@@ -2,28 +2,71 @@
   <v-container fluid>
     <v-row justify="center">
       <v-sheet width="100%">
-        <v-form @submit.prevent>
+        <v-form>
           <v-container fluid>
-            <v-row>
-              <AttendeeRow />
-            </v-row>
+            <template v-for="n in rowCount" :key="n">
+              <v-row>
+                <AttendeeRow :row-index="n" @info-change="updateInfo" />
+              </v-row>
+            </template>
             <v-row justify="center">
-              <v-btn type="submit">Submit</v-btn>
+              <v-btn type="submit" @click.prevent="pushToDatabase">
+                Submit
+              </v-btn>
             </v-row>
           </v-container>
         </v-form>
       </v-sheet>
     </v-row>
+    <pre>{{ example }}</pre>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-import { defineComponent } from "vue";
+import {
+  type IAttendeeData,
+  type IAttendeeDataIndex,
+  type IAddRowResponse,
+} from "@/interfaces";
 </script>
 
 <script lang="ts">
-export default defineComponent({
-  data: () => ({}),
+export default defineNuxtComponent({
+  data: () => ({
+    rowCount: 3,
+    information: [] as IAttendeeDataIndex[],
+    example: "",
+  }),
+  methods: {
+    updateInfo(info: IAttendeeData, row: number) {
+      const match = this.information.find((element) => element.index == row);
+      if (match) {
+        match.data = info;
+      } else {
+        this.information.push({ index: row, data: info });
+      }
+    },
+    async pushToDatabase() {
+      for (const row of this.information) {
+        if (row.data.firstName != "" && row.data.lastName != "") {
+          try {
+            const response = await $fetch<IAddRowResponse>("/api/addRow", {
+              method: "POST",
+              body: row,
+            });
+            if (response.status === "success") {
+              console.log("write successful");
+            } else {
+              console.error("write failed");
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
+    },
+  },
+  async mounted() {},
 });
 </script>
 
