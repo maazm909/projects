3bd0290158636c437cb30ef00ba2ebe23f7489f8
Maaz Mohammad <maazm909@gmail.com>
2024-05-16 00:45:51 -0500

    Updated router link path due to rename

--- conventionapp/convention-app/pages/registration/index.vue
+++ conventionapp/convention-app/pages/registration/index.vue
@@ -2,7 +2,7 @@
   <div class="flex-col justify-center container">
     <h1>REGISTRATION</h1>
     <div class="flex-row gap-1 links">
-      <RouterLink to="/registration/dayof">
+      <RouterLink to="/registration/day-of">
         <v-btn>Day Of</v-btn>
       </RouterLink>
       <RouterLink to="/registration/online">
