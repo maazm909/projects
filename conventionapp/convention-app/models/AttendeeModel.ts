ac819786c7c65989dd0e541edfd1f33d809c63c5
Maaz Mohammad <maazm909@gmail.com>
2024-05-16 00:45:29 -0500

    Created attendee model and search page

---
+++ conventionapp/convention-app/models/AttendeeModel.ts
@@ -0,0 +1,18 @@
+import Prisma from "@prisma/client";
+
+export default class AttendeeModel {
+  public static hydrateAttendee(
+    initialData?: Partial<Prisma.Attendee>,
+  ): Prisma.Attendee {
+    initialData = initialData ?? {};
+    return {
+      id: initialData.id ?? -1,
+      firstName: initialData.firstName ?? "",
+      lastName: initialData.lastName ?? "",
+      age: initialData.age ?? 0,
+      email: initialData.email ?? "",
+      gender: initialData.gender ?? "",
+      checkedIn: initialData.checkedIn ?? false,
+    };
+  }
+}
---
+++ conventionapp/convention-app/pages/registration/attendee-search.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fluid>
+    <template v-for="(row, index) in currentData" :key="row">
+      <AttendeeRow
+        :information="row"
+        :row-index="index"
+        @info-change="updateInfo"
+      />
+    </template>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import Prisma from "@prisma/client";
+import { alertTypes } from "~/interfaces";
+</script>
+
+<script lang="ts">
+export default defineNuxtComponent({
+  data: () => ({
+    //...
+    currentData: [] as Prisma.Attendee[],
+    updateResponse: "",
+    loading: false,
+    alertType: undefined as alertTypes,
+    isAlertOpen: false,
+  }),
+  methods: {
+    updateInfo(info: Prisma.Attendee, row: number) {
+      if (this.currentData[row].checkedIn != info.checkedIn) {
+        // update database, flip checkedIn based on id
+      }
+    },
+  },
+  created() {
+    // get all attendees, load them into currentData
+  },
+});
+</script>
+
+<style></style>
