af1c6efc0876045ed4ba5615dc23e68d679575df
Maaz Mohammad <maazm909@gmail.com>
2024-06-18 01:10:05 -0500

    Remove quran registration link

--- conventionapp/convention-app/layouts/default.vue
+++ conventionapp/convention-app/layouts/default.vue
@@ -8,9 +8,6 @@
         <RouterLink to="/registration">
           <v-btn>Registration</v-btn>
         </RouterLink>
-        <RouterLink to="/quranregistration">
-          <v-btn>Quran Registration</v-btn>
-        </RouterLink>
         <RouterLink to="/bazaar">
           <v-btn>Bazaar</v-btn>
         </RouterLink>
